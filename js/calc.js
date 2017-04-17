f90d492bce32291ad72806f412def5f45013f01b
DelMonroe <delenor@gmail.com>
2017-04-17 13:24:46 -0500

    Calculator project: added folders and files

---
+++ js/calc.js
@@ -0,0 +1,25 @@
+/**
+ * Created by DelMonroe on 4/17/17.
+ */
+(function () {
+    "use strict";
+
+    function btn_1() {
+
+    }function btn_1() {
+
+    }function btn_1() {
+
+    }function btn_1() {
+
+    }function btn_1() {
+
+    }function btn_1() {
+
+    }function btn_1() {
+
+    }function btn_1() {
+
+    }
+
+})();
