0e28b82758a504cb9eb48838e01e6b3a6672c295
DelMonroe <delenor@gmail.com>
2017-04-17 14:38:36 -0500

    Calculator project: added folders and files

--- js/calc.js
+++ js/calc.js
@@ -5,20 +5,108 @@
     "use strict";
 
     function btn_1() {
+        var val;
+        var b1 = document.getElementById("I-1");
+        b1.setAttribute("value", "1");
+        val = b1.getAttribute("value");
 
-    }function btn_1() {
+    }
+    var btn1 = document.getElementById("btn_1");
+    btn1.addEventListener("click", btn_1);
 
-    }function btn_1() {
 
-    }function btn_1() {
+    function btn_2() {
+        var b2= document.getElementById("I-1");
+        b2.setAttribute("value", "2");
 
-    }function btn_1() {
+    }
+    var btn2 = document.getElementById("btn_2");
+    btn2.addEventListener("click", btn_2);
 
-    }function btn_1() {
+    function btn_3() {
+        var b3 = document.getElementById("I-1");
+        b3.setAttribute("value", "3");
+    }
+    var btn3 = document.getElementById("btn_3");
+    btn3.addEventListener("click", btn_3);
 
-    }function btn_1() {
+    function btn_4() {
+        var b4 = document.getElementById("I-2");
+        b4.setAttribute("value", "+");
 
-    }function btn_1() {
+    }
+    var btn4 = document.getElementById("btn_4");
+    btn4.addEventListener("click", btn_4);
+
+    function btn_5() {
+        var b1 = document.getElementById("I-1");
+        b1.setAttribute("value", "4");
+
+    }
+
+    function btn_6() {
+        var b1 = document.getElementById("I-1");
+        b1.setAttribute("value", "4");
+
+    }
+
+    function btn_7() {
+        var b1 = document.getElementById("I-1");
+        b1.setAttribute("value", "4");
+
+    }
+
+    function btn_8() {
+        var b1 = document.getElementById("I-1");
+        b1.setAttribute("value", "4");
+
+    }
+
+    function btn_9() {
+        var b1 = document.getElementById("I-1");
+        b1.setAttribute("value", "4");
+
+    }
+
+    function btn_10() {
+        var b1 = document.getElementById("I-1");
+        b1.setAttribute("value", "4");
+
+    }
+
+    function btn_11() {
+        var b1 = document.getElementById("I-1");
+        b1.setAttribute("value", "4");
+
+    }
+
+    function btn_12() {
+        var b1 = document.getElementById("I-1");
+        b1.setAttribute("value", "4");
+
+    }
+
+    function btn_13() {
+        var b1 = document.getElementById("I-1");
+        b1.setAttribute("value", "4");
+
+    }
+
+    function btn_14() {
+        var b1 = document.getElementById("I-1");
+        b1.setAttribute("value", "4");
+
+    }
+
+    function btn_15() {
+        var b1 = document.getElementById("I-1");
+        b1.setAttribute("value", "4");
+
+    }
+
+    function btn_16() {
+        var b1 = document.getElementById("I-1");
+        b1.setAttribute("value", "4");
 
     }
 
